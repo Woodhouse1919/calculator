fca2f1689adc346c3a958a66ec74ff8e81382242
Woodhouse <pariswoodhouse@hotmail.com>
2020-02-09 23:24:42 -0800

    Added update display on button click

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
 
-
-function add(a ,b) {
-    let answer =  a + b;
-    display.textContent = answer;
-};
-
-function subtract(a, b) {
-    let answer = a - b;
-    display.textContent = answer;
-};
-
-function multiply(a, b) {
-    let answer = a * b;
-    display.textContent = answer;
-};
-
-function divide(a, b) {
-    let answer = a / b;
-    display.textContent = answer;
-};
+const add = (a ,b) => a + b;
+const subtract = (a, b) => a - b; 
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 function operate(operator, a, b) {
-    if (operator === add && typeof a === 'number' && typeof b === 'number') {
-        add(a, b);
-    } else if (operator === subtract && typeof a === 'number' && typeof b === 'number') {
-        subtract(a, b);
-    } else if (operator === multiply && typeof a === 'number' && typeof b === 'number') {
-        multiply(a, b);
-    } else if (operator === divide && typeof a === 'number' && typeof b === 'number') {
-        divide(a, b);
+    if (operator === add) {
+        let answer = add(a, b);
+        return answer;
+    } else if (operator === subtract) {
+        let answer = subtract(a, b);
+        return answer;
+    } else if (operator === multiply) {
+        let answer = multiply(a, b);
+        return answer;
+    } else if (operator === divide) {
+        let answer = divide(a, b);
+        return answer;
     } 
 } 
+// clicked button gets put into display
+let displayValue = [];
+const updateDisplay = function(number) {
+    displayValue = displayValue + number;
+};
+
+numbers.forEach((number) => {
+    number.addEventListener('click', (e) => {
+        updateDisplay(e.target.textContent);
+        display.textContent = displayValue;
+    });
+});
