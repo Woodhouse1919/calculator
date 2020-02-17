6318d6f392fed2ee5d5cdb699fe40132b60c6154
Woodhouse <pariswoodhouse@hotmail.com>
2020-02-17 09:41:30 -0800

    need to fix decimals & multi op functions

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const operators = document.querySelectorAll('.operator')
 const equals = document.querySelector('#equals')
 const decimal = document.querySelector('#decimal');
 
+backspaceBtn()
+clearBtn();
+
+let decimalAllowed = true;
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b; 
 const multiply = (a, b) => a * b;
@@ -15,42 +20,42 @@ function operate(operator, a, b) {
     if (operator === add) {
         let answer = add(a, b);
         if (b == []) {
-        } else { // this number if statement checks if answer includes decimal then shortens it to 4 places
+        } else { // this number if statement checks if answer includes decimal then shortens it to 2 places
             if (Number.isInteger(answer) === false) {
-                display.textContent = answer.toFixed(4);
+                display.textContent = answer.toFixed(2);
          } else { 
              display.textContent = answer;
-        };
+         };
         }; 
     } else if (operator === subtract) {
         let answer = subtract(a, b);
         if (b == []) {
         } else {
             if (Number.isInteger(answer) === false) {
-                display.textContent = answer.toFixed(4);
+                display.textContent = answer.toFixed(2);
          } else { 
              display.textContent = answer;
-        };
+         };
         }; 
     } else if (operator === multiply) {
         let answer = multiply(a, b);
         if (b == []) {
         } else {
             if (Number.isInteger(answer) === false) {
-                display.textContent = answer.toFixed(4);
+                display.textContent = answer.toFixed(2);
          } else { 
              display.textContent = answer;
-        };
+         };
         }; 
     } else if (operator === divide) {
         let answer = divide(a, b);
         if (b == []) {
         } else {
            if (Number.isInteger(answer) === false) {
-               display.textContent = answer.toFixed(4);
+               display.textContent = answer.toFixed(2);
         } else {
             display.textContent = answer;
-        };
+         };
         }; 
     }; 
 };
@@ -65,32 +70,46 @@ const updateDisplay = function(number) {
 
 numbers.forEach((number) => {
     number.addEventListener('click', (e) => {
-        updateDisplay(e.target.textContent);
-        display.textContent = displayValue;
+        if (e.target.textContent === '.') {
+            decimalAllowed = false;
+            updateDisplay(e.target.textContent);
+            display.textContent = displayValue;
+            decimal.disabled = true;
+        } else {
+            updateDisplay(e.target.textContent);
+            display.textContent = displayValue;
+        }
+        
+        
     });
 });
-        
+    ;
 
 // clears display when C button clicked
+function clearBtn() {
 clear.addEventListener('click', () => {
+    decimal.disabled = false;
     displayValue = [];
     display.textContent = displayValue;
-})
+})};
 
 // removes last number when backspace is clicked
+function backspaceBtn() {
 backspace.addEventListener('click', () => {
     displayValue = displayValue.slice(0, -1);
     display.textContent = displayValue;
-})
+})};
 
 // main calculator "brain"
 operators.forEach((operator) => {
     operator.addEventListener('click', (e) => {
+        decimal.disabled = false;
         let firstNum = displayValue;
         let operator = e.target.textContent;
         displayValue = [];
             equals.addEventListener('click', () => {
             secondNum = displayValue;
+            decimal.disabled = false;
                 if (operator === '+') {
                     operate(add, Number(firstNum), Number(secondNum));
                     secondNum = [];
